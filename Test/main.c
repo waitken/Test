cdabecad787da61410d06ccac146252239361484
Wayne <wayne@Large-iMac.local>
2012-09-27 20:05:43 -0700

    Initial Commit

---
+++ Test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Test
+//
+//  Created by Wayne on 9/27/12.
+//  Copyright (c) 2012 Wayne. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
